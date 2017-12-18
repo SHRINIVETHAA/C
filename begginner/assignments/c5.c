973a1d11327faf459e6d31e03e7ed82eef2111ff
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2017-12-18 21:17:48 +0530

    Create c5.c

---
+++ begginner/assignments/c5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b,c;
+	printf("enter the numbers:\n");
+	scanf("%d%d%d",&a,&b,&c);
+	if((a>b)&&(a>c))
+	{
+		printf("%d",a);
+	
+	}
+	else if((b>a)&&(b>c))
+	{
+		printf("%d",b);
+	}
+	else if((c>a)&&(c>b))
+	{
+		printf("%d",c);
+	}
+	return 0;
+}
