5dd9be5cf827a1bca9c625f7c38695778dfe37a7
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2017-12-18 14:56:40 +0530

    Add files via upload

---
+++ begginner/assignments/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+   char a[20]="sachin tendulkar";
+   char b[20]="little master";
+    int c=18426;
+    float d=44.83;
+    int e=463;
+    printf("%s\n",a);
+    printf("%s\n",b);
+    printf("%d\n",c);
+    printf("%.2f\n",d);
+    printf("%d\n",e);
+    return 0;
+}
+
+
