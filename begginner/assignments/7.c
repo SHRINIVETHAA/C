a7b506671008c1b5d2e8568c6d6d92ddef41484b
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2017-12-19 07:32:21 +0000

    Create 7.c

---
+++ begginner/assignments/7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+	if(argc!=0)
+	{
+		printf("argument count is  not equal to zero\n");
+	}
+	
+	{printf("%s\n",argv[0]);
+	 printf("%s",argv[1]);
+             
+			
+	}
+
+return 0;
+	
+}
