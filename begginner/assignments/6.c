ac06b3d75c4d0b9c0ace80763775ca69e4fd82b0
SHRINIVETHAA <34641392+SHRINIVETHAA@users.noreply.github.com>
2017-12-19 06:46:49 +0000

    Create 6.c

---
+++ begginner/assignments/6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int a=9;
+	while(a>0)
+	{
+		
+	a=a+2;
+	printf("%d",a);
+	a=a-1;
+	printf("%d",a);
+	a=a-4;
+	printf("%d",a);
+	break;
+		
+	}
+	return 0;
+}
